3d6341de0199d3564e054eccfae4804c24f3af6c
Tasnim Ahmed <tasnimahmed@iut-dhaka.edu>
2020-10-16 21:36:45 +0600

    Navigation Done

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "./src/screens/HomeScreen";
+import NotificationScreen from "./src/screens/NotificationScreen";
+import ProfileScreen from "./src/screens/ProfileScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
 import SignInScreen from "./src/screens/SignInScreen";
 
 import { AuthContext, AuthProvider } from "./src/providers/AuthProvider";
-
-const HomeStack = createStackNavigator();
+import { Entypo, AntDesign, Ionicons } from "@expo/vector-icons";
 const AuthStack = createStackNavigator();
+const HomeTab = createMaterialBottomTabNavigator();
+const AppDrawer = createDrawerNavigator();
 
-const HomeStackScreen = () => {
+const AppDrawerScreen = () => {
   return (
-    <HomeStack.Navigator initialRouteName="Home">
-      <HomeStack.Screen name="Home" component={HomeScreen} />
-    </HomeStack.Navigator>
+    <AppDrawer.Navigator>
+      <AppDrawer.Screen name="Home" component={HomeTabScreen} />
+      <AppDrawer.Screen name="Profile" component={ProfileScreen} />
+    </AppDrawer.Navigator>
+  );
+};
+
+const HomeTabScreen = () => {
+  return (
+    <HomeTab.Navigator initialRouteName="Home">
+      <HomeTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Entypo name="home" color="white" size={26} />
+            ) : (
+              <AntDesign name="home" color="white" size={22} />
+            ),
+        }}
+      />
+      <HomeTab.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          tabBarLabel: "Notifications",
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+              <Ionicons name="ios-notifications" size={26} color="white" />
+            ) : (
+              <Ionicons
+                name="ios-notifications-outline"
+                size={22}
+                color="white"
+              />
+            ),
+        }}
+      />
+    </HomeTab.Navigator>
   );
 };
 
@@ -42,7 +84,7 @@ function App() {
       <AuthContext.Consumer>
         {(auth) => (
           <NavigationContainer>
-            {auth.IsLoggedIn ? <HomeStackScreen /> : <AuthStackScreen />}
+            {auth.IsLoggedIn ? <AppDrawerScreen /> : <AuthStackScreen />}
           </NavigationContainer>
         )}
       </AuthContext.Consumer>
